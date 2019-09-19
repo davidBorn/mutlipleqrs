da2631af692634a69c68300fffea0a6c2150fd92
davidBorn <david.born25@gmail.com>
2019-09-18 22:19:10 -0600

    second commit

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,10 +1,23 @@
 module WelcomeHelper
-   
-    require 'rqrcode'
+  require 'rqrcode'
 
-    qrcode = RQRCode::QRCode.new("https://www.youtube.com/watch?v=god7hAPv8f0")
-   
-    end
+
+
+  def qrgenerate(number)
+      qrcode = RQRCode::QRCode.new(number.to_s)
+      # NOTE: showing with default options specified explicitly
+      png = qrcode.as_png(
+      resize_gte_to: false,
+      resize_exactly_to: false,
+      fill: 'white',
+      color: 'black',
+      size: 240,
+      border_modules: 0,
+      module_px_size: 6,
+      file: 'app/assets/images/qrcodepng' + number + '.png' # path to write
+      )
+  end
+end
     
     
 
